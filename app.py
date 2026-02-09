879b7992cdfe239c230eea0046b2e737851bca63
Asap-Gorbe <dmb_plata@outlook.com>
2026-02-09 20:30:42 +0330

    Inital commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "<h2>About page</h2>"
+
+app.run(debug=True)
